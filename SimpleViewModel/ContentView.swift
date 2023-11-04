90f61c6b98307a7a1a1c65e42e8eed605acbd843
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2023-11-04 04:20:15 -0700

    Initial Xcode project.

---
+++ SimpleViewModel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleViewModel
+//
+//  Created by Eric Chamberlain on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleViewModel/SimpleViewModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleViewModelApp.swift
+//  SimpleViewModel
+//
+//  Created by Eric Chamberlain on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleViewModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
