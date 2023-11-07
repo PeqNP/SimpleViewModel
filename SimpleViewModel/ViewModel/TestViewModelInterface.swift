20486545ae6830b2bbd6e5d1fb98eebb0497b789
Eric Chamberlain <eric.j.chamberlain@protonmail.com>
2023-11-07 08:44:03 -0800

    Added logic to wait async expectations.

--- SimpleViewModel/ViewModel/TestViewModelInterface.swift
+++ SimpleViewModel/ViewModel/TestViewModelInterface.swift
@@ -26,19 +26,45 @@ class TestViewModelInterface<T: ViewModel> {
         return self
     }
     
-    func expect(_ expected: [T.Output], wait: DispatchTime = .now() + .milliseconds(1), file: StaticString = #file, line: UInt = #line) {
-        DispatchQueue.main.asyncAfter(deadline: wait) { [weak self] in
-            self?.expect(expected, file: file, line: line)
+    /// Expect `[Output]` from a given `send` action.
+    ///
+    /// If multiple `Output`s are expected, they _must_ be performed at the same time within the same thread.
+    func expect(_ expected: [T.Output], wait seconds: TimeInterval = 2, file: StaticString = #file, line: UInt = #line) {
+        let waiter = XCTWaiter()
+        let expectation = XCTestExpectation(description: "Outputs are equal")
+        waitForOutputs(expectation, wait: seconds)
+        let result = waiter.wait(for: [expectation], timeout: seconds)
+        switch result {
+        case .completed:
+            XCTAssertEqual(expected, outputs, file: file, line: line)
+        case .timedOut:
+            XCTFail("Expected Outputs, but none were returned")
+        default:
+            XCTFail("Invalid result returned from `wait` - \(result)", file: file, line: line)
         }
+        outputs = []
     }
-
-    func _expect(_ expected: [T.Output], file: StaticString = #file, line: UInt = #line) {
-        guard !outputs.isEmpty else {
-            XCTAssert(false, "No outputs were recorded. This may be caused by `respond` not being called or `respond` was called asynchronously.", file: file, line: line)
+    
+    private func waitForOutputs(_ expectation: XCTestExpectation, wait seconds: TimeInterval, iteration: Double = 0) {
+        let iterationsPerSecond: Double = 10
+        // `2` ensures this finishes before `waiter.wait` finishes in `expect`
+        guard iteration < (iterationsPerSecond * seconds) - 1 else {
             return
         }
-        XCTAssertEqual(outputs, expected, file: file, line: line)
-        outputs = []
+        // No need to wait 100 milliseconds on the first iteration. This is especially true if the expectation has no async calls.
+        let interval: DispatchTimeInterval = iteration == 0 ? .seconds(0) : .milliseconds(100)
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) { [weak self] in
+            guard let self else {
+                expectation.fulfill()
+                return
+            }
+            if !self.outputs.isEmpty {
+                expectation.fulfill()
+            }
+            else {
+                self.waitForOutputs(expectation, wait: seconds, iteration: iteration + 1)
+            }
+        }
     }
 
     private func respond(_ output: T.Output) {
